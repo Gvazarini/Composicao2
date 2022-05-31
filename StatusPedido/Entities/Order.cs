951cf013bc245b6f8ddd04e34ba98c555ac8b6ca
Gabriel Vazarini <camaravazarini@gmail.com>
2022-05-31 13:10:36 -0300

    Composicao

---
+++ StatusPedido/Entities/Order.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StatusPedido.Entities.Enuns;
+using System.Globalization;
+
+namespace StatusPedido.Entities
+{
+    class Order
+    {
+        private object CulturalInfo;
+
+        public DateTime Momment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order() { 
+        }
+
+        public Order(DateTime momment, OrderStatus orderstatus, Client client)
+        {
+            Momment = momment;
+            Status = orderstatus;
+            Client = client;
+        }
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+        public double Total()
+        {
+            double sum = 0;
+            foreach (OrderItem item in Items)
+            {
+                sum += item.SubTotal();
+            }
+            return sum;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Order momment: " + Momment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.Append("Order Status:" + Status);
+            sb.Append("Client: " + Client);
+            sb.Append("Order Items:");
+                foreach (OrderItem item in Items)
+            {
+                sb.Append(item.ToString());
+            }
+            sb.AppendLine("Total Price: " + Total().ToString("F2", CultureInfo.InvariantCulture));
+            return sb.ToString();
+        }
+    }
+}
---
+++ StatusPedido/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatusPedido.Entities
+{
+    class Product
+    {
+        public double Price { get; set; }
+        public string Name { get; set; }
+
+        public Product(double price, string name)
+        {
+            Price = price;
+            Name = name;
+        }
+    }
+}
